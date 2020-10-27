96c380d6783d518175211a753c78884f8e50c13d
smccowan3 <68258345+smccowan3@users.noreply.github.com>
2020-10-27 12:02:34 +1100

    Updates 2710

---
+++ Assets/CharacterRoster.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterRoster : MonoBehaviour
+{
+    public List<Character> CharList = new List<Character>(15);
+    // Start is called before the first frame update
+    void Start()
+    {
+    }
+}
